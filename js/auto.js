2c1d854d3b2364074d1c5cee02f6300b459f48f4
Vazy <86739701+ZzVAZYzZ@users.noreply.github.com>
2023-02-25 17:21:13 +0700

    changed size

--- js/auto.js
+++ js/auto.js
@@ -27,3 +27,16 @@ setInterval(function(){
         bt=0;
     }
 },800)
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenEl = document.querySelectorAll('.hidden');
+hiddenEl.forEach((el) => observer.observe(el));
